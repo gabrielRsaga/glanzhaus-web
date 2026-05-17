5a237454e68c0719734273e896dbf8c5dd20e625
gabrielRsaga <gabrielRsaga@users.noreply.github.com>
2026-05-17 18:50:52 +0000

    deploy: 36d677796145591b014f44f39ac0449841e6847a

---
+++ chunk-KTTY62CT.js
@@ -0,0 +1,1 @@
+var i=[{label:"Instagram",icon:"pi pi-instagram",href:"https://www.instagram.com/glanzhaus.ch/"},{label:"LinkedIn",icon:"pi pi-linkedin",href:"https://www.linkedin.com/company/glanz-haus/"},{label:"Facebook",icon:"pi pi-facebook",href:"https://www.facebook.com/profile.php?id=61574993188419"}];export{i as a};
